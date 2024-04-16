63db85dd3d2a2ce63b632176f7406f082d5d910e
Arushisharma1610 <arushisharma2003bly@gmail.com>
2024-04-16 12:38:23 +0530

    code updated

--- pyramid.cpp
+++ pyramid.cpp
@@ -1,6 +1,37 @@
-#include<iostream>
+// C++ code to demonstrate star pattern
+#include <iostream>
 using namespace std;
 
-int main(){
-    
+// Function to demonstrate printing pattern
+void pypart2(int n)
+{
+	// Number of spaces
+	int i, j, k = n;
+
+	// Outer loop to handle number of rows
+	// n in this case
+	for (i = 1; i <= n; i++) {
+
+		// Inner loop for columns
+		for (j = 1; j <= n; j++) {
+
+			// Condition to print star pattern
+			if (j >= k)
+				cout << "5 ";
+			else
+				cout << " ";
+		}
+		k--;
+		cout << "\n";
+	}
 }
+
+// Driver Code
+int main()
+{
+	int n = 8;
+	// Function Call
+	pypart2(n);
+	return 0;
+}
+
