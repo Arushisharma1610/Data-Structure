e741cf32bddbe2b92564187a44eefb5b90e2f65e
Arushisharma1610 <arushisharma2003bly@gmail.com>
2024-09-08 15:18:47 +0530

    upadted code

--- map/mapIntro.cpp
+++ map/mapIntro.cpp
@@ -20,5 +20,25 @@ int main(){
     // this insertion will replace the current value 1 with the new vale
     m["mera"] = 2;
 
+    // Searching
+    cout<< m["mera"]<<endl;
+    cout<<m.at("arushi")<<endl;
+
+    // it will create a new entry for this unknown key "abcd" at the 0th place/index
+    cout<< m["abcd"]<<endl;
+
+    // it will give output 0 if the key exist unless it will produce error if key does not exist
+    cout<< m.at("abcd" )<<endl;
+
+    // size
+    cout<<m.size()<<endl;
+
+    // to check presence
+    cout<< m.count("arushi")<<endl;     //it will give 1 if key is present , unless will give 0
+
+    // erase
+    m.erase("arushi");
+    cout<< m.size()<<endl;
+
     return 0;
 }
